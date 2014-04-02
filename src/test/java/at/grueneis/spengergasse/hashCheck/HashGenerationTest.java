df83f9d1b15f0da318867ae5538f8b0a4d03dab1
Felix Eisler <eis14324@spengergasse.at>
2014-04-02 18:27:08 +0200

    moved hashcheck to own package, deleted all classes from lesson_plan

--- src/test/java/at/grueneis/spengergasse/hashCheck/HashGenerationTest.java
+++ src/test/java/at/grueneis/spengergasse/hashCheck/HashGenerationTest.java
@@ -2,59 +2,47 @@ package at.grueneis.spengergasse.hashCheck;
 
 import static org.junit.Assert.*;
 
-import at.grueneis.spengergasse.lesson_plan.domain.BasePersistable;
 import org.junit.Before;
 import org.junit.Test;
 
 public class HashGenerationTest {
-	private BasePersistable base0;
-	private BasePersistable base1;
+	private Hashable hashable0;
+	private Hashable hashable1;
     private HashGenerator hashGenerator;
 	
 	@Before
 	public void init(){
         hashGenerator = new HashGenerator();
-		base0 = new BasePersistable() {
-			private int integer  = 100;
-			private String string = "Name";
-			@Override
-			public String[] getAllAttributesAsString() {
-				return new String[]{""+this.getId(), ""+integer, string};
-			}
-		};
-		base0.setId(0l);
-		base0.setMd5Hash(hashGenerator.generateMd5Hash(base0));
-		
-		base1 = new BasePersistable() {
-			private int integer  = 100;
-			private String string = "Name";
-			@Override
-			public String[] getAllAttributesAsString() {
-				return new String[]{""+this.getId(), ""+integer, string};
-			}
-		};
-		base1.setId(0l);
-        base1.setMd5Hash(hashGenerator.generateMd5Hash(base1));
+		hashable0 = new HashableImplementation();
+		hashable0.setId(0l);
+		hashable0.updateMd5Hash();
+
+		hashable1 = new HashableImplementation();
+        hashable1 = new HashableImplementation();
+        hashable1.setId(hashable0.getId());
+        hashable1.updateMd5Hash();
+
     }
 	
 	@Test
 	public void hashEqualWithoutChanges(){
-        base0.setMd5Hash(hashGenerator.generateMd5Hash(base0));
-		String hash0 = base0.getMd5Hash();
-        base0.setMd5Hash(hashGenerator.generateMd5Hash(base0));
-		assertTrue(hash0.equals(base0.getMd5Hash()));
+        hashable0.setMd5Hash(hashGenerator.generateMd5Hash(hashable0));
+		String hash0 = hashable0.getMd5Hash();
+        hashable0.setMd5Hash(hashGenerator.generateMd5Hash(hashable0));
+		assertTrue(hash0.equals(hashable0.getMd5Hash()));
 	}
 	
 	@Test
 	public void hashEquivalent(){
-		assertTrue(base0.getMd5Hash().equals(base1.getMd5Hash()));
+        //both hashables have the same attributes
+		assertTrue(hashable0.getMd5Hash().equals(hashable1.getMd5Hash()));
 	}
 	
 	@Test
 	public void hashNotEquivalentAfterChanges(){
-		base0.setId(10l);
-        base0.setMd5Hash(hashGenerator.generateMd5Hash(base0));
-		assertFalse(base0.getMd5Hash().equals(base1.getMd5Hash()));
+		hashable0.setId(10l);
+        hashable0.setMd5Hash(hashGenerator.generateMd5Hash(hashable0));
+		assertFalse(hashable0.getMd5Hash().equals(hashable1.getMd5Hash()));
 	}
 	
 }
